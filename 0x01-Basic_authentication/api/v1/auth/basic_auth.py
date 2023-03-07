ff2a2b5ff4c9a56d6ae99d17fc98c906ef196392
Gody <adekaji@gmail.com>
2023-03-07 14:17:42 +0000

    basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Definition of class BasicAuth
+"""
+import base64
+from .auth import Auth
+from typing import TypeVar
+
+from models.user import User
+
+
+class BasicAuth(Auth):
+    """ Implement Basic Authorization protocol methods
+    """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization header for a Basic
+        Authorization
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        token = authorization_header.split(" ")[-1]
+        return token
